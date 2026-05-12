5fa048d06ab6f1549f556f0f36c5ab3e10121611
PavelTzv <pavel.tzvetkov@icloud.com>
2026-05-12 15:11:13 +0200

    Page min export

---
+++ src/app/marketing-materials/page.tsx
@@ -0,0 +1,7 @@
+export default function MarketingMaterialsPage() {
+  return (
+    <main>
+      <h1>Marketing Materials</h1>
+    </main>
+  );
+}
---
+++ src/app/photographs/page.tsx
@@ -0,0 +1,7 @@
+export default function PhotographsPage() {
+  return (
+    <main>
+      <h1>Photographs</h1>
+    </main>
+  );
+}
